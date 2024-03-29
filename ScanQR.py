1bab836c31836d2edf5efd644b98a613dd1984c0
egasol <simon.nasfeldt@gmail.com>
2024-03-29 13:19:36 +0100

    Added button to read cards

--- ScanQR.py
+++ ScanQR.py
@@ -1,18 +1,31 @@
 import cv2
-from time import time
+from gpiozero import Button
+
+
+# Increasing IMAGES_MAX increased the likelyhood that card is correctly read, but increases compute time.
+IMAGES_MAX = 5
 
 
 class Scanner:
     def __init__(self):
         self.video_capture = cv2.VideoCapture(0)
         self.qr_detector = cv2.QRCodeDetector()
+        self.button = Button(2)
+        self.images = []
 
     def read_input(self):
         while True:
-            _, image = self.video_capture
-            data, bbox, _ = self.qr_detector.detectAndDecode(image)
+            _, image = self.video_capture.read()
+            self.images.append(image)
 
-            if bbox is not None:
-                if data:
-                    print("data:", data)
-                    return data
+            if len(self.images) > IMAGES_MAX:
+                self.images.pop(0)
+
+            if self.button.is_pressed:
+                for image_previous in self.images:
+                    data, bbox, _ = self.qr_detector.detectAndDecode(image_previous)
+                    if bbox is not None:
+                        if data:
+                            return data
+
+                return None
